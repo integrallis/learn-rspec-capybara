1481fec4c06880ed6fa913b3915c0701b4839813
Danny Whalen <daniel.r.whalen@gmail.com>
2013-04-26 14:22:50 -0700

    Setup bootstrap-sass assets and structure app layout

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '2.3.1.1'
 end
 
 group :test, :development do
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def navbar_list_item(text, path, opts={})
+    return if opts[:hide]
+    cls = request.fullpath == path ? 'active' : ''
+    content_tag :li, :class => cls do
+      link_to text, path, opts
+    end
+  end
 end
