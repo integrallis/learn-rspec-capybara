450e959ee55d45cda36cd81622f2353f187d0079
Danny Whalen <daniel.r.whalen@gmail.com>
2013-04-26 14:13:20 -0700

    Setup capybara

--- Gemfile
+++ Gemfile
@@ -14,3 +14,7 @@ end
 group :test, :development do
   gem 'rspec-rails', '~> 2.13.0'
 end
+
+group :test do
+  gem 'capybara', '~> 2.1.0'
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+require 'capybara/rspec'
+
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
